50a458da9e7f01269e08073cdd089da94dbc3396
Priyosaha <107949592+Priyosaha@users.noreply.github.com>
2022-07-08 21:54:57 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,6 +2,9 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+
+yourArray=[1,56,true,"priyosaha","Ilovecoding"];
+console.log(yourArray);
 // code end
 
 module.exports = yourArray
